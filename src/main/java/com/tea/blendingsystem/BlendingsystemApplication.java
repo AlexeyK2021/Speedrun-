b68297e24296a8a484c8da89a43a07ed486cf8ee
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2024-05-14 17:02:06 +0300

    init

---
+++ src/main/java/com/tea/blendingsystem/BlendingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.tea.blendingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlendingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlendingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tea/blendingsystem/BlendingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tea.blendingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlendingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
